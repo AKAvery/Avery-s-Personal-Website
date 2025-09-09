3c8951ea67105b9058a51bf1fe146a3db12b974a
Avery Allen <atxavery@gmail.com>
2025-09-09 17:33:11 -0500

    Add routes & filesystem handler so all pages work

--- api/index.py
+++ api/index.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, abort
+from jinja2 import TemplateNotFound
 
 app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
+# Home
 @app.get("/")
 def home():
-    return render_template("index.html")     # your existing template
+    return render_template("index.html")
 
-# if your pages link to /static/*
-@app.get("/static/<path:filename>")
-def static_files(filename):
-    return send_from_directory(app.static_folder, filename)
+# Explicit simple pages
+@app.get("/about")
+def about():
+    return render_template("about.html")
+
+@app.get("/projects")
+def projects():
+    return render_template("projects.html")
+
+# Generic fallback: serve any template by path
+# Supports /page  -> templates/page.html
+#         /foo/bar -> templates/foo/bar.html
+@app.get("/<path:page>")
+def serve_page(page):
+    for name in (page, f"{page}.html"):
+        try:
+            return render_template(name)
+        except TemplateNotFound:
+            pass
+    abort(404)
