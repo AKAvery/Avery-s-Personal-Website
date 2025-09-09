3fd1643d385524266d1312d653cd62c3496c0e96
Avery Allen <atxavery@gmail.com>
2025-09-09 16:49:23 -0500

    Vercel Additions

--- app.py
+++ app.py
@@ -28,3 +28,4 @@ def andersonCodingClub():
 
 if __name__ == '__main__':
     app.run(port = 4000, debug=True)
+    #app.run(debug=False,host='0.0.0.0')
---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
+
+@app.get("/")
+def home():
+    return render_template("index.html")   # your existing template
+
+# If you link /static/* directly:
+@app.get("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory(app.static_folder, filename)
